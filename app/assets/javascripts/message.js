38022025a1ef08869ebd9b027b1b99d6883bfd85
tomtech59 <shimanokan@gmail.com>
2019-10-28 19:19:43 +0900

    add Ajax function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,49 @@
 $(function(){
-  $('.new_message').on('submit', function(e){
+  function buildMessage(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+    return html;
+  }
+
+  // function scroll(){
+    // $('.messages').animate({scrollTop:$('.message')[0].scrollHeight},"fast");
+  // }
+
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message);
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight},"fast");
+      $('.form__submit').prop('disabled',false)
+    })
+    .fail(function(message){
+      alert('投稿できぬ');
+    })
+    return false;
   })
-})
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.user_name @message.user.name
+json.created_at @message.created_at
+json.content @message.content
