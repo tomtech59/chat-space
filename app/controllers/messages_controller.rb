4c6252f10644f3e7d8d6ff81e9729d61488e42d5
tomtech59 <shimanokan@gmail.com>
2019-11-09 12:18:00 +0900

    trial commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
       end
     else
       @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
+      flash.now[:alert] = 'メッセージを入力してください'
       render :index
     end
   end
